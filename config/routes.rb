d6e243869cb79bee1b9feb93e668d4c58bf74c13
rjlmacfarlane <rjl.macfarlane@gmail.com>
2021-02-03 23:14:44 +0000

    Implement feature/about-page

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,10 @@ Rails.application.routes.draw do
 
   resources :orders, only: [:create, :show]
 
+  resource :about, only: [:show] do
+    root to: 'about#show'
+  end
+
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
