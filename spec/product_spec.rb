b09e672bdd7ff4bba2c6a226239a90900530dbe8
rjlmacfarlane <rjl.macfarlane@gmail.com>
2021-02-08 21:44:42 +0000

    Implement Category/Qty cannot be blank tests

--- spec/product_spec.rb
+++ spec/product_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Product, type: :model do
   describe 'Validations' do
+
     it 'saves succesfully when all fields are valid' do
       @category = Category.create(id: 1, name: "Bakery")
       @product = Product.create(id: 1, name: "Loaf Pan", price_cents: 10000, quantity: 1, category_id: 1)
@@ -15,5 +16,20 @@ RSpec.describe Product, type: :model do
 
       expect(@product.errors.full_messages.include?("Name can't be blank")).to be true
     end
+
+    it "has 'Quantity can't be blank' error when quantity is null" do
+      @category = Category.create(id: 1, name: "Bakery")
+      @product = Product.create(id: 1, name: "Loaf Pan", price_cents: 10000, quantity: nil, category_id: 1)
+
+      expect(@product.errors.full_messages.include?("Quantity can't be blank")).to be true
+    end
+
+    it "has 'Category can't be blank' error when category is nil" do
+      @category = nil
+      @product = Product.create(id: 1, name: "Loaf Pan", price_cents: 10000, quantity: 10, category_id: 1)
+
+      expect(@product.errors.full_messages.include?("Category can't be blank")).to be true
+    end
+
   end
 end
